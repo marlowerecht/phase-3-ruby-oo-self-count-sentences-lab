936a615c4f04f5a0e41dc53caa5821aca2976f13
Marlowe Recht <recht.marlowe@gmail.com>
2023-01-31 16:48:25 +0100

    commit tree 287428bc88c1424a164c607b56d9f2969dd8cba0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") == true
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?") == true
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    number_of_sentences = self.split(/[!+|.+|?+]/)
+    number_of_sentences.length
+    # binding.pry
   end
 end
